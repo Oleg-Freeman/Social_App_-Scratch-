1c45bcb947d631e389acec4038e4b212010c629b
Oleg Freeman <av.ssf4@gmail.com>
2020-02-22 17:54:45 +0200

    Comment routes
    Added routes for get, delete & update comments

--- backend/routes/screams.js
+++ backend/routes/screams.js
@@ -34,16 +34,9 @@ router.route('/add').post(ensureAuthenticated, (req, res) => {
 });
 
 router.route('/:id').get((req, res) => {
-  Scream.findById(req.params.id).populate('comments', 'body').exec((err, comments) => {
-    if (err) return res.status(400).json('Error: ' + err);
-    else res.json('Screams with comments: ' + comments);
-  });
-  // .then(screams => {
-  //   console.log('Screams:' + screams);
-
-  //   res.json(screams);
-  // })
-  // .catch(err => res.status(400).json('Error: ' + err));
+  Scream.findById(req.params.id)
+    .then(scream => res.json(scream))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/:id').delete(ensureAuthenticated, (req, res) => {
@@ -54,18 +47,18 @@ router.route('/:id').delete(ensureAuthenticated, (req, res) => {
 
 router.route('/update/:id').post(ensureAuthenticated, (req, res) => {
   Scream.findById(req.params.id)
-    .then(screams => {
+    .then(scream => {
       const { error } = bodyValidation(req.body);
       if (error) return res.status(400).send(error.details[0].message);
 
       // screams.screamId = req.body.screamId;// _id: new mongoose.Types.ObjectId().toHexString(),
       // screams.userHandle = req.user.handle;
-      screams.body = req.body.body;
+      scream.body = req.body.body;
       // screams.likeCount = req.body.likeCount;
       // screams.commentCount = req.body.commentCount;
       // screams.comments = req.body.comments;
 
-      screams.save()
+      scream.save()
         .then(() => res.json('Scream updated!'))
         .catch(err => res.status(400).json('Error: ' + err));
     })
@@ -77,13 +70,58 @@ router.route('/comments/add/:id').post(ensureAuthenticated, (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   Scream.findById(req.params.id)
-    .then(screams => {
-      screams.comments.push({ body: req.body.body, userHandle: req.user.handle });
+    .then(scream => {
+      scream.comments.push({ body: req.body.body, userHandle: req.user.handle });
+      scream.commentCount = ++scream.commentCount;
 
-      screams.save()
+      scream.save()
         .then(() => res.json('Comment added!'))
         .catch(err => res.status(400).json('Error: ' + err));
     }).catch(err => res.status(400).json('Error: ' + err));
 });
+
+// Get comment of Scream by ID
+router.route('/comments/:screamId/:commentId')
+  .get((req, res) => {
+    Scream.findById(req.params.screamId)
+      .then(scream => {
+        const queriedComment = scream.comments.filter(comment => {
+          return comment._id.toString() === req.params.commentId;
+        });
+
+        res.json(queriedComment);
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+router.route('/comments/del/:screamId/:commentId')
+  .delete(ensureAuthenticated, (req, res) => {
+    Scream.findById(req.params.screamId)
+      .then(scream => {
+        const toDelete = scream.comments.findIndex(comment => {
+          return comment._id.toString() === req.params.commentId;
+        });
+        scream.comments.splice(toDelete, 1);
+
+        scream.save()
+          .then(() => res.json('Comment deleted'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+router.route('/comments/update/:screamId/:commentId')
+  .post(ensureAuthenticated, (req, res) => {
+    Scream.findById(req.params.screamId).where()
+      .then(scream => {
+        scream.comments.filter(comment => {
+          if (comment._id.toString() === req.params.commentId) comment.body = req.body.body;
+        });
+
+        scream.save()
+          .then(() => res.json('Comment updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      }).catch(err => res.status(400).json('Error: ' + err));
+  });
 
 module.exports = router;
