6e8b414a85a330aeb680d59d6556258f6e5845ab
Oleg Freeman <av.ssf4@gmail.com>
2020-03-03 17:57:20 +0200

    React setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/navbar.component';
 import ExercisesList from './components/exercises-list.component';
@@ -11,14 +12,17 @@ import CreateUser from './components/create-user.component';
 function App() {
   return (
     <Router>
-      <div className="container">
-        <Navbar />
-        <br/>
-        <Route path="/" exact component={ExercisesList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
-        <Route path="/user" component={CreateUser} />
-      </div>
+      <Switch>
+        <div className="container">
+          <Navbar />
+          <br/>
+          <Route path="/" exact component={ExercisesList} />
+          <Route path="/edit/:id" component={EditExercise} />
+          <Route path="/create" component={CreateExercise} />
+          <Route path="/user" component={CreateUser} />
+        </div>
+      </Switch>
+
     </Router>
   );
 }
